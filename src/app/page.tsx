619a26d123862d2f49f586892b717020a1acfcc9
Helen Kriklivets <helenakriklivets.cad@gmail.com>
2024-11-03 16:29:08 -0500

    feat: initial commit, fix configs and structure

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </main>
+    </div>
+  );
+}
