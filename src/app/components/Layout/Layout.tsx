6232e5d4c7c33a5a942ea19cb85acf67494d9769
Helen Kriklivets <helenakriklivets.cad@gmail.com>
2024-11-03 18:17:10 -0500

    feat: Task2 create header component

--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import {ReactNode} from "react";
+import Header from "@components/Header/Header";
 
 interface LayoutProps {
     children: ReactNode;
@@ -11,6 +12,7 @@ export default function Layout({
     }: Readonly<LayoutProps>) {
     return (
         <>
+            <Header />
             <main>
                 {children}
             </main>
