88ba231db3af0bf27632489d5f5bb3a530ae6aac
Helen Kriklivets <helenakriklivets.cad@gmail.com>
2024-11-03 19:32:12 -0500

    feat: Task2 displayed data on UI, installed tw-merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import "@/app/globals.css";
 import {fetchJokes} from "@utils/jokesFetcher";
 import Head from "next/head";
+import {twMerge} from "tailwind-merge";
 
 interface JokeFullListData {
     error: boolean;
@@ -22,15 +23,24 @@ interface PageProps {
 }
 
 let cachedJokesData: JokeFullListData | null = null;
+const beforeElmBorder = twMerge(
+    'before:content-[""]',
+    'before:-left-5 before:-right-5 before:bottom-0',
+    'before:absolute before:h-px before:bg-black',
+    'before:block',
+    'last:before:hidden'
+);
 
 export async function getServerSideProps() {
-    try {
-        cachedJokesData = await fetchJokes(5);
-    } catch (err: unknown) {
-        console.error('Failed to fetch jokes list');
+    if (!cachedJokesData) {
+        try {
+            cachedJokesData = await fetchJokes(5);
+        } catch (err: unknown) {
+            console.error('Failed to fetch jokes list');
+        }
     }
 
-    return { props: { jokeList: cachedJokesData }};
+    return { props: { jokeList: cachedJokesData?.jokes ?? [] }};
 }
 
 const TITLE_PAGE = 'Jokes list';
@@ -42,7 +52,18 @@ const Page = ({ jokeList }: PageProps) => {
                 <title>{TITLE_PAGE}</title>
                 <meta property="og:title" content={TITLE_PAGE} key="title"/>
             </Head>
-            Page Content
+            <ol className="list-decimal">
+                {jokeList?.map(({type, joke, setup, delivery}: JokeData, index: number) => (
+                    <li key={`joke-${index}`} className={twMerge('relative py-2', beforeElmBorder)}>
+                        {type === 'single' ? (joke) : (
+                            <>
+                                <p>- {setup}</p>
+                                <p>- {delivery}</p>
+                            </>
+                        )}
+                    </li>
+                ))}
+            </ol>
         </div>
     );
 };
